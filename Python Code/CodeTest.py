8ec116988b062990e4b95340776033b1104d2dd7
Edgar <edirc98@gmail.com>
2021-01-25 11:52:16 +0100

    First code tests in Python

--- Python Code/CodeTest.py
+++ Python Code/CodeTest.py
@@ -1,1 +1,36 @@
 print("Hello World")
+numero = 1000
+print(numero)
+numero = 10.7
+print(numero)
+numero = "Hola"
+print(numero)
+
+palabras = "Hola me lo paso muy bien"
+cn1 = "Cadena 1"
+cn2 = "Cadena 2"
+
+Cadenas = cn1 + cn2
+
+cortada = cn1[1:4]
+thelast = cn1[:-1]
+print(thelast)
+print(len(Cadenas))
+
+h = 4
+c = "-"
+cT = ""
+i = 0
+
+while i < h*2:
+    if i < h:
+        cT += c
+    else:
+        cT = cT[:-1]
+
+    i += 1
+    print(cT)
+
+    
+name = input("Say your name: ")
+print("Hello " + name + "!")
