88432620748795b7b8e0eec183a5fda174895617
edirc98 <edirc98@gmail.com>
2021-02-03 11:49:08 +0100

    Finished base code, some automatas do not move as expected

--- Python Code/Game of Life/GameOfLife.py
+++ Python Code/Game of Life/GameOfLife.py
@@ -1,6 +1,8 @@
 import pygame
 import numpy as np
 import time
+
+
 pygame.init()
 
 width,heigh = 1000,1000
@@ -10,18 +12,24 @@ bg = 25,25,25
 screen.fill(bg)
 
 #Numero de celldas y dimension
-nxC,nyC = 25,25
+nxC,nyC = 50,50
 dimCW = width / nxC
 dimCH = heigh / nyC
 
-#Estado de las celdas (Matriz) ALive = 1, Dead = 0
+#Estado de las celdas (Matriz) Alive = 1, Dead = 0
 gameState = np.zeros((nxC,nyC))
 
 PauseExecution = False
 ExitGame = False
+#Automata Palo
+gameState[5,3] = 1
+gameState[5,4] = 1
+gameState[5,5] = 1
+
+
+
 #Bucle de ejecucion
 while not ExitGame:
-    
     newGameState = np.copy(gameState)
     #Refresco de pantalla
     screen.fill(bg)
@@ -35,35 +43,43 @@ while not ExitGame:
         #Detecion de raton
         mouseClick = pygame.mouse.get_pressed()
 
-        if sum(mouseClick) > 0:
+        #Dar vida a celda
+        if mouseClick[0] == 1:
             posX,posY = pygame.mouse.get_pos()
             celX,celY = int(np.floor(posX / dimCW)), int(np.floor(posY/dimCH))
             newGameState[celX,celY] = 1
+        #Matar a celda
+        elif mouseClick[2] == 1:
+            posX,posY = pygame.mouse.get_pos()
+            celX,celY = int(np.floor(posX / dimCW)), int(np.floor(posY/dimCH))
+            newGameState[celX,celY] = 0
+
         #RatonCentral = Exit main game loop  
         if mouseClick[1] == 1:
             ExitGame = True
 
-    for y in range(0,nxC):
-        for x in range(0,nyC):
-            #Calculo del numero de vecinos de cada una de la celdas
-            n_neigh = gameState[(x - 1) % nxC, (y - 1) % nyC] + \
-                    gameState[(x)     % nxC, (y - 1) % nyC] + \
-                    gameState[(x + 1) % nxC, (y - 1) % nyC] + \
-                    gameState[(x - 1) % nxC, (y)     % nyC] + \
-                    gameState[(x + 1) % nxC, (y)     % nyC] + \
-                    gameState[(x - 1) % nxC, (y + 1) % nyC] + \
-                    gameState[(x)     % nxC, (y + 1) % nyC] + \
-                    gameState[(x + 1) % nxC, (y + 1) % nyC]
+    for x in range(0,nxC):
+        for y in range(0,nyC):
+            if not PauseExecution:
+                #Calculo del numero de vecinos de cada una de la celdas
+                n_neigh = gameState[(x - 1) % nxC, (y - 1) % nyC] + \
+                          gameState[(x)     % nxC, (y - 1) % nyC] + \
+                          gameState[(x + 1) % nxC, (y - 1) % nyC] + \
+                          gameState[(x - 1) % nxC, (y)     % nyC] + \
+                          gameState[(x + 1) % nxC, (y)     % nyC] + \
+                          gameState[(x - 1) % nxC, (y + 1) % nyC] + \
+                          gameState[(x)     % nxC, (y + 1) % nyC] + \
+                          gameState[(x + 1) % nxC, (y + 1) % nyC]
 
 
-            #Reglas de juego
-            #(1)Una celda muerta(state = 0), con 3 vecinas vivas, "revive"
-            if gameState[x,y] == 0 and n_neigh == 3:
-                newGameState[x,y] == 1
-
-            #(2)Una celda viva con menos de 2 vecionos o mas de 3, "muere"
-            elif gameState[x,y] == 1 and (n_neigh < 2 or n_neigh > 3):
-                newGameState [x,y] == 0
+                #Reglas de juego
+                #(1)Una celda muerta(state = 0), con 3 vecinas vivas, "revive"
+                if gameState[x,y] == 0 and n_neigh == 3:
+                    newGameState[x,y] = 1
+                
+                #(2)Una celda viva con menos de 2 vecionos o mas de 3, "muere"
+                elif gameState[x,y] == 1 and (n_neigh < 2 or n_neigh > 3):
+                    newGameState [x,y] = 0
 
             #Cada un de los cuadraditos con sus 4 coordenadas
             poly = [((x)  * dimCW, y * dimCH),
@@ -79,4 +95,7 @@ while not ExitGame:
                 pygame.draw.polygon(screen,(255,255,255),poly,0)
             
             gameState = np.copy(newGameState)
+
     pygame.display.flip()
+
+
