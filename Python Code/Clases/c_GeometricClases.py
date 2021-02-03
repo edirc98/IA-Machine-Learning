e7777565bb6ae18a099145ee597c57aaa882066f
edirc98 <edirc98@gmail.com>
2021-02-03 10:38:03 +0100

    Added getters and setters in geometric classes

--- Python Code/Clases/c_GeometricClases.py
+++ Python Code/Clases/c_GeometricClases.py
@@ -1,32 +1,77 @@
 import math
 
 class Square:
-<<<<<<< HEAD
-=======
 
->>>>>>> f7ca382d4d89378769d10acb3e9e7ff1f4e34e04
+    #Constructor
     def __init__(self,b,h):
         self.base = b
         self.height = h
+        self.__area = self.__area()
 
-    def area(self):
+    #Setters
+    def setSquareData(self,newBase,newHeight):
+        self.base = newBase
+        self.height = newHeight
+    def setSquareBase(self,newBase):
+        self.base = newBase
+    def setSquareHeight(self,newHeight):
+        self.height = newHeight
+
+    #Getters
+    def getSquareBase(self):
+        return self.base
+    def getSquareHeight(self):
+        return self.height
+    def getSquareArea(self):
+        return self.__area
+    #Functions
+    def __area(self):
         return self.base * self.height
 
 
 class Triangle:
+    #Constructor
     def __init__(self,base,height):
         self.base = base
         self.height = height
+        self.__area = self.__area()
     
-    def area(self):
+    #Setters
+    def setTriangleBase(self,newBase):
+        self.base = newBase
+    def setTriangleHeigth(self,newHeigth):
+        self.base = newHeigth
+
+    #Getters
+    def getTriangleBase(self):
+        return self.base
+    def getTriangleHeight(self):
+        return self.height
+    def getTriangleArea(self):
+        return self.__area
+
+    #Functions
+    def __area(self):
         return ((self.base * self.height) / 2)
 
 
 class Circle:
+    #Constructor
     def __init__(self,radius):
         self.radius = radius
+        self.__area = self.__area()
     
-    def area(self):
+    #Setters
+    def setCircleRadius(self,newRadius):
+        self.radius = newRadius
+    #Getters
+    def getCircleRadius(self):
+        return self.radius
+    def getCircleArea(self):
+        return self.__area
+
+    #Functions
+    def __area(self):
         return math.pi * self.radius**2
 
 s1 = Square(10,10)
